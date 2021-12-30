61548c8f72c43726a5f6d8a31352da1b565a3fc8
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-12-30 08:33:12 -0300

    22. Pipe Group

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ const initialBirdPosition = {
 }
 
 let bird = null
+let pipes = null
 
 let pipeHorizontalDistance = 0
 
@@ -45,28 +46,31 @@ function preload() {
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0)
-  
+
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0)
   bird.body.gravity.y = 400
 
-  for (let i = 0; i < PIPES_TO_RENDER; i++){
-    const upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1)
-    const lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0)
+  pipes = this.physics.add.group()
+
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1)
+    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0)
     placePipe(upperPipe, lowerPipe)
   }
 
+  pipes.setVelocityX(-VELOCITY)
 
   this.input.on('pointerdown', flap)
 }
 
-function update(){
+function update() {
   if (bird.y > config.height || bird.y < 0 - bird.height) {
     console.log('game over')
     restartBirdPosition()
   }
 }
 
-function placePipe(upperPipe, lowerPipe){
+function placePipe(upperPipe, lowerPipe) {
   pipeHorizontalDistance += 400
   let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange)
   let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance)
@@ -77,8 +81,6 @@ function placePipe(upperPipe, lowerPipe){
   lowerPipe.x = upperPipe.x
   lowerPipe.y = upperPipe.y + pipeVerticalDistance
 
-  upperPipe.body.velocity.x = -VELOCITY
-  lowerPipe.body.velocity.x = -VELOCITY
 }
 
 function restartBirdPosition() {
